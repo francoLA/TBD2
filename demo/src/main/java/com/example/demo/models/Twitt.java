e20c8652838348804d288b1b25c4b8abc8158b4b
franco <franco.labra@usach.cl>
2019-06-04 14:39:56 -0400

    modelo de usuario y twitt actualizados

--- demo/src/main/java/com/example/demo/models/Twitt.java
+++ demo/src/main/java/com/example/demo/models/Twitt.java
@@ -1,6 +1,7 @@
 package com.example.demo.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
@@ -13,13 +14,19 @@ import org.springframework.data.elasticsearch.annotations.Document;
 @NoArgsConstructor
 @Document(indexName = "tweets", type = "tweets",shards = 2)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(value = {"createdAt"}, allowGetters = true)
 public class Twitt {
 
     @Id
-    private String id;
+    private Long id;
     private String text;
     private String lang;
     private User user;
+    private String country;
+    private String countryCode;
+    private String state;
+    private Double geoLatitude;
+    private Double geoLongitude;
 
     @SerializedName("retweet_count")
     private int retweetCount;
@@ -27,20 +34,25 @@ public class Twitt {
     @SerializedName("favorite_count")
     private int favoriteCount;
 
-    public Twitt(String id, String text, String lang, User user, int retweetCount, int favoriteCount) {
+    public Twitt(Long id, String text, String lang, User user, int retweetCount, int favoriteCount, String country, String countryCode, String state, Double geoLatitude, Double geoLongitude) {
         this.id = id;
         this.text = text;
         this.lang = lang;
         this.user = user;
         this.retweetCount = retweetCount;
         this.favoriteCount = favoriteCount;
+        this.country = country;
+        this.countryCode = countryCode;
+        this.state = state;
+        this.geoLatitude = geoLatitude;
+        this.geoLongitude = geoLongitude;
     }
 
-    public String getId() {
+    public Long getId() {
         return this.id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -84,16 +96,24 @@ public class Twitt {
         this.favoriteCount = favoriteCount;
     }
 
-    @Override
-    public String toString() {
-        return "Tweet{" +
-                "id=" + id +
-                ", text='" + text + '\'' +
-                ", lang='" + lang + '\'' +
-                ", user=" + user +
-                ", retweetCount=" + retweetCount +
-                ", favoriteCount=" + favoriteCount +
-                '}';
-    }
+    public String getCountry() { return country; }
+
+    public String getCountryCode() { return countryCode; }
+
+    public String getState() { return state; }
+
+    public Double getGeoLatitude() { return geoLatitude; }
+
+    public Double getGeoLongitude() { return geoLongitude; }
+
+    public void setCountry(String country) { this.country = country; }
+
+    public void setCountryCode(String countryCode) { this.countryCode = countryCode; }
+
+    public void setState(String state) { this.state = state; }
+
+    public void setGeoLatitude(Double geoLatitude) { this.geoLatitude = geoLatitude; }
+
+    public void setGeoLongitude(Double geoLongitude) { this.geoLongitude = geoLongitude; }
 
 }
